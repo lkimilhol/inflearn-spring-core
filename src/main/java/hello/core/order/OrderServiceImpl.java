6da9b2799a97b0e1b7b830408cb52ded8220e9ef
lkimihol <lkimilhol@gmail.com>
2022-02-01 15:17:30 +0900

    롬복과 최신 트랜드

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,22 +3,20 @@ package hello.core.order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import lombok.RequiredArgsConstructor;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     // final 키워드를 붙임으로써 생성자에서만 셋팅이 가능하므로 불변이다.
     // fianl 키워드를 붙임으로써 초기화가 되지 않으면 컴파일 에러!
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
